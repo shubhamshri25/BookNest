5ec25683b3c2f11abf41ecdbc880912c05273d3b
shubham <shubhshri25gmail.com>
2024-09-22 12:42:46 +0530

    seeing user data on all orders placed

--- Frontend/src/components/navbar/Navbar.jsx
+++ Frontend/src/components/navbar/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
   }
 
   if (isLoggedIn === true && role === "admin") {
-    navLinks.splice(4, 1);
+    navLinks.splice(3, 1);
   } else {
     navLinks.splice(5, 1);
   }
--- Frontend/src/pages/SeeUserData.jsx
+++ Frontend/src/pages/SeeUserData.jsx
@@ -10,8 +10,8 @@ const SeeUserData = ({ userDivData, userDiv, setUserDiv }) => {
       <div
         className={`${userDiv} top-0 left-0 h-screen w-full flex items-center justify-center`}
       >
-        <div className="bg-white rounded p-4 w-[80%] md:w-[50%] lg:w-[40%]">
-          <div className="flex items-center justify-center">
+        <div className="bg-zinc-300 rounded text-gray-600  p-4 w-[80%] md:w-[50%] lg:w-[40%]">
+          <div className="flex items-center justify-between">
             <h1 className=" text-2xl font-semibold ">User information</h1>
             <button onClick={() => setUserDiv("hidden")}>
               <RxCross1 />
@@ -27,10 +27,16 @@ const SeeUserData = ({ userDivData, userDiv, setUserDiv }) => {
 
           <div className="mt-4">
             <label htmlFor="">
-              Email:{" "}
-              <span className="font-semibold">{userDivData.email}</span>
+              Email: <span className="font-semibold">{userDivData.email}</span>
             </label>
           </div>
+
+          <div className="mt-4">
+            <label htmlFor="">
+              Address: <span className="font-semibold">{userDivData.address}</span>
+            </label>
+          </div>
+
         </div>
       </div>
     </>
